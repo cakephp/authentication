727f15e025f131003ae555945a2ac8c778bc141b
Florian Krämer <florian.kraemer@cakedc.com>
2016-09-28 02:12:18 +0200

    Doc blocks for tests

--- tests/TestCase/Authentication/FormAuthenticatorTest.php
+++ tests/TestCase/Authentication/FormAuthenticatorTest.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         4.0.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
 namespace Auth\Test\TestCase\Middleware\Authentication;
 
 use Cake\ORM\TableRegistry;
@@ -11,11 +23,21 @@ use Zend\Diactoros\ServerRequestFactory;
 class FormAuthenticatorTest extends TestCase
 {
 
+    /**
+     * Fixtures
+     *
+     * @var array
+     */
     public $fixtures = [
         'core.auth_users',
         'core.users'
     ];
 
+    /**
+     * setUp
+     *
+     * @return void
+     */
     public function setUp()
     {
         parent::setUp();
