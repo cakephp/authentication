6cc2642739d99675bd49f224626b1112d0b76cc9
Florian Krämer <florian.kraemer@cakedc.com>
2016-11-14 21:25:03 +0100

    Adding the identity object

---
+++ src/Authentication/Identity.php
@@ -0,0 +1,44 @@
+<?php
+namespace Auth\Authentication;
+
+class Identity {
+
+    /**
+     * Identity data
+     *
+     * @var mixed
+     */
+    protected $_identity;
+
+    /**
+     * array|\ArrayAccess
+     */
+    public function __construct($identity)
+    {
+        if (!is_array($identity) && !is_object($identity)) {
+            throw new \InvalidArgumentException(sprintf('First arg must be an array or object. `%s` ', gettype($identity)));
+        }
+
+        $this->_identity = $identity;
+    }
+
+    public function set($path, $value)
+    {
+        Hash::insert($this->_identity, $path, $value);
+    }
+
+    public function get($path = null)
+    {
+        if ($path === null) {
+            return $this->_identity;
+        }
+
+        return Hash::get($this->_identity, $path);
+    }
+
+    public function has($path)
+    {
+        return (bool)Hash::get($this->_identity, $path);
+    }
+
+}
