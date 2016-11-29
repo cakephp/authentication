39d4dbb443e89a6dad66299029c40cfcb31cd810
ADmad <admad.coder@gmail.com>
2016-11-30 00:15:54 +0530

    Remove LogTrait from AbstractIdentifier

--- src/Authentication/Identifier/AbstractIdentifier.php
+++ src/Authentication/Identifier/AbstractIdentifier.php
@@ -14,13 +14,11 @@
 namespace Auth\Authentication\Identifier;
 
 use Cake\Core\InstanceConfigTrait;
-use Cake\Log\LogTrait;
 
 abstract class AbstractIdentifier implements IdentifierInterface
 {
 
     use InstanceConfigTrait;
-    use LogTrait;
 
     /**
      * Default configuration
