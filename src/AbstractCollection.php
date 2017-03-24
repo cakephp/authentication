48e9265c5b3c987aa3e25120de7dffbf94d7b9e5
Robert Pustułka <r.pustulka@robotusers.com>
2017-03-24 14:22:18 +0100

    Created AbstractCollection based on ObjectRegistry.

---
+++ src/AbstractCollection.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Authentication;
+
+use Cake\Core\InstanceConfigTrait;
+
+/**
+ * Description of AbstractCollection
+ *
+ * @author Robert Pustułka <r.pustulka@robotusers.com>
+ */
+abstract class AbstractCollection extends \Cake\Core\ObjectRegistry
+{
+    use InstanceConfigTrait;
+
+    protected $_items = [];
+
+    /**
+     * Config array.
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [];
+
+    /**
+     * Constructor
+     *
+     * @param array $config Configuration
+     */
+    public function __construct(array $config = [])
+    {
+        $this->setConfig($config);
+
+        foreach ($config as $key => $value) {
+            if (is_int($key)) {
+                $this->load($value);
+                continue;
+            }
+            $this->load($key, $value);
+        }
+    }
+
+    /**
+     * Returns all objects.
+     *
+     * @return array
+     */
+    public function getAll()
+    {
+        return $this->_loaded;
+    }
+}
