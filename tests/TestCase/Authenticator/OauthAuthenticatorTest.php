a6a657ed4021a1146e4eef5e81e1492edf60da2d
Michael Hoffmann <mh@scherer-software.de>
2017-04-03 02:40:18 +0200

    Add authenticate test

--- tests/TestCase/Authenticator/OauthAuthenticatorTest.php
+++ tests/TestCase/Authenticator/OauthAuthenticatorTest.php
@@ -17,10 +17,12 @@ use Authentication\Identifier\IdentifierCollection;
 use Authentication\Result;
 use Authentication\Test\TestCase\AuthenticationTestCase as TestCase;
 use Cake\Http\Response;
+use Cake\Http\ServerRequest;
 use Cake\Http\ServerRequestFactory;
 use Cake\Routing\Router;
 use SocialConnect\Auth\Service;
 use SocialConnect\Common\Http\Client\Curl;
+use SocialConnect\OAuth2\Accesstoken;
 use SocialConnect\OAuth2\Provider\Github;
 use SocialConnect\Provider\Session\Session;
 
@@ -51,6 +53,84 @@ class OauthAuthenticatorTest extends TestCase
         ]);
 
         $this->sessionMock = $this->createMock(Session::class);
+
+        Router::reload();
+        Router::scope('/', function ($routes) {
+            $routes->connect('/users/login', ['controller' => 'Users', 'action' => 'login']);
+            $routes->connect('/users/callback', ['controller' => 'Users', 'action' => 'callback']);
+            $routes->fallbacks();
+        });
+    }
+
+    /**
+     * testAuthenticate
+     *
+     * @return void
+     */
+    public function testAuthenticate()
+    {
+        $entity = new \stdClass;
+        $entity->username = 'mariano';
+
+        $accessToken = $this->createMock(Accesstoken::class);
+        $accessToken->method('getToken')
+            ->willReturn('1234');
+
+        $provider = $this->createMock(Github::class);
+        $provider->method('getIdentity')
+            ->with($accessToken)
+            ->willReturn($entity);
+        $provider->method('makeAuthUrl')
+            ->willReturn('example.com');
+        $provider->method('getAccessTokenByRequestParameters')
+            ->willReturn($accessToken);
+
+        $authService = $this->createMock(Service::class);
+        $authService->method('getprovider')
+            ->willReturn($provider);
+        $authService->method('getConfig')
+            ->willReturn([
+                'redirectUri' => 'http://app.dev/users/callback',
+                'provider' => [
+                    'github' => [
+                        'applicationId' => '1234',
+                        'applicationSecret' => '1234',
+                    ]
+                ]
+            ]);
+
+        $request = new ServerRequest([
+            'params' => [
+                'plugin' => null,
+                'controller' => 'Users',
+                'action' => 'callback',
+                '_ext' => null,
+                'pass' => [
+                    'github'
+                ]
+            ],
+            'environment' => [
+                'REQUEST_URI' => '/users/callback/github/'
+            ]
+        ]);
+        $response = new Response();
+
+        $oauth = new OauthAuthenticator($this->identifiers, [
+            'loginUrl' => [
+                'controller' => 'Users',
+                'action' => 'login'
+            ],
+            'redirectUrl' => [
+                'controller' => 'Users',
+                'action' => 'callback'
+            ],
+            'authService' => $authService
+        ]);
+
+        $result = $oauth->authenticate($request, $response);
+
+        $this->assertInstanceOf('\Authentication\Result', $result);
+        $this->assertEquals(Result::SUCCESS, $result->getCode());
     }
 
     /**
