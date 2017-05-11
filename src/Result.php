c0357c429bb2261d58bcd037d646da2ad3d7c7b6
Robert Pustułka <r.pustulka@robotusers.com>
2017-05-11 09:10:54 +0200

    Move constants to ResultInterface.

--- src/Result.php
+++ src/Result.php
@@ -21,36 +21,6 @@ use InvalidArgumentException;
 class Result implements ResultInterface
 {
     /**
-     * General Failure
-     */
-    const FAILURE = 0;
-
-    /**
-     * Failure due to identity not being found.
-     */
-    const FAILURE_IDENTITY_NOT_FOUND = -1;
-
-    /**
-     * Failure due to invalid credential being supplied.
-     */
-    const FAILURE_CREDENTIAL_INVALID = -2;
-
-    /**
-     * Failure due to other circumstances.
-     */
-    const FAILURE_OTHER = -3;
-
-    /**
-     * The authentication credentials were not found in the request.
-     */
-    const FAILURE_CREDENTIALS_NOT_FOUND = -4;
-
-    /**
-     * Authentication success.
-     */
-    const SUCCESS = 1;
-
-    /**
      * Authentication result code
      *
      * @var int
