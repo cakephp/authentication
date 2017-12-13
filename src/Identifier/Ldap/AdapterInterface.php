f133f6688e58891e543418fffc45052c9a1570d0
inoas <mail@inoas.com>
2017-12-13 16:23:09 +0100

    add since 1.0.0

--- src/Identifier/Ldap/AdapterInterface.php
+++ src/Identifier/Ldap/AdapterInterface.php
@@ -9,6 +9,7 @@
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
  * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         1.0.0
  * @license       https://opensource.org/licenses/mit-license.php MIT License
  */
 namespace Authentication\Identifier\Ldap;
