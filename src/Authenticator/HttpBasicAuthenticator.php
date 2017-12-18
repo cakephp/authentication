921c8286ebbfc3fc28ce595fbc59a1373bc7486e
inoas <mail@inoas.com>
2017-12-18 23:53:09 +0100

    strict

--- src/Authenticator/HttpBasicAuthenticator.php
+++ src/Authenticator/HttpBasicAuthenticator.php
@@ -91,7 +91,7 @@ class HttpBasicAuthenticator extends AbstractAuthenticator implements StatelessI
     protected function loginHeaders(ServerRequestInterface $request)
     {
         $server = $request->getServerParams();
-        $realm = $this->getConfig('realm') ?: $server['SERVER_NAME'];
+        $realm = $this->getConfig('realm') === null ? $server['SERVER_NAME'] : $this->getConfig('realm');
 
         return ['WWW-Authenticate' => sprintf('Basic realm="%s"', $realm)];
     }
