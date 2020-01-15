0eb35577ed01f220b4b741fd667e8fd7a49e1fe3
ADmad <ADmad@users.noreply.github.com>
2020-01-16 02:12:29 +0530

    Avoid unnecessary file operations.
    This plugin doesn't have bootstrap file or routes file or console commands.

--- src/Plugin.php
+++ src/Plugin.php
@@ -18,8 +18,28 @@ namespace Authentication;
 use Cake\Core\BasePlugin;
 
 /**
- * Plugin class for CakePHP 3.6.0 plugin collection.
+ * Plugin class for CakePHP.
  */
 class Plugin extends BasePlugin
 {
+    /**
+     * Do bootstrapping or not
+     *
+     * @var bool
+     */
+    protected $bootstrapEnabled = false;
+
+    /**
+     * Load routes or not
+     *
+     * @var bool
+     */
+    protected $routesEnabled = false;
+
+    /**
+     * Console middleware
+     *
+     * @var bool
+     */
+    protected $consoleEnabled = false;    
 }
