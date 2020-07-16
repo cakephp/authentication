913dc9e580405bc2c98e7fb9b114e9fcf280c0f1
andrii-pukhalevych <a.puhalevich@gmail.com>
2020-07-16 13:55:35 +0300

    testLoadInvalidAuthenticatorObject message merge

--- tests/TestCase/AuthenticationServiceTest.php
+++ tests/TestCase/AuthenticationServiceTest.php
@@ -135,7 +135,7 @@ class AuthenticationServiceTest extends TestCase
      * testLoadInvalidAuthenticatorObject
      *
      * @expectedException \RuntimeException
-     * @expectedExceptionMessage Authenticator class `TestApp\Authentication\Authenticator\InvalidAuthenticator` must implement `Authentication\Authenticator\AuthenticatorInterface`.
+     * @expectedExceptionMessage Authenticator class `TestApp\Authentication\Authenticator\InvalidAuthenticator` must implement \Auth\Authentication\AuthenticatorInterface
      */
     public function testLoadInvalidAuthenticatorObject()
     {
