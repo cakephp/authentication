ae9b90c5b9fe37facfbfec6ae365c81b565a00c7
Mark Story <mark@mark-story.com>
2026-06-12 23:29:06 -0400

    Fix an open redirect weakness in getLoginRedirect()
    Because of how browsers handle the `Location` header, values beginning
    with `\` can be leveraged to create redirect targets on other domains.
    
    Thanks to Volker Dusch and the PHP Ecosystem security team for reporting this.

--- tests/TestCase/AuthenticationServiceTest.php
+++ tests/TestCase/AuthenticationServiceTest.php
@@ -822,7 +822,7 @@ class AuthenticationServiceTest extends TestCase
         );
     }
 
-    public function testGetLoginRedirect(): void
+    public function testGetLoginRedirectInvalid(): void
     {
         $service = new AuthenticationService([
             'unauthenticatedRedirect' => '/users/login',
@@ -861,6 +861,39 @@ class AuthenticationServiceTest extends TestCase
         );
         $this->assertSame(
             '/path/with?query=string',
+            $service->getLoginRedirect($request),
+        );
+
+        $request = ServerRequestFactory::fromGlobals(
+            ['REQUEST_URI' => '/login'],
+            ['redirect' => '/\\evil.com'],
+        );
+        $this->assertNull(
+            $service->getLoginRedirect($request),
+        );
+
+        $request = ServerRequestFactory::fromGlobals(
+            ['REQUEST_URI' => '/login'],
+            ['redirect' => '\\/\\evil.com'],
+        );
+        $this->assertNull(
+            $service->getLoginRedirect($request),
+        );
+
+        $request = ServerRequestFactory::fromGlobals(
+            ['REQUEST_URI' => '/login'],
+            ['redirect' => '\\evil.com/path'],
+        );
+        $this->assertSame(
+            '/evil.com/path',
+            $service->getLoginRedirect($request),
+        );
+
+        $request = ServerRequestFactory::fromGlobals(
+            ['REQUEST_URI' => '/login'],
+            ['redirect' => '\\\\evil.com/path'],
+        );
+        $this->assertNull(
             $service->getLoginRedirect($request),
         );
     }
